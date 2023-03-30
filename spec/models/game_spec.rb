8c29fbdaa4da274ff5eacf226673265e75939142
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-03-30 19:35:56 +0600

    Edit test game_spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -92,20 +92,24 @@ RSpec.describe Game, type: :model do
     context '.answer_current_question!' do
       it 'correct answer' do
         expect(game_w_questions.answer_current_question!('d')).to be_truthy
+        expect(game_w_questions.status).to eq(:in_progress)
       end
 
       it 'wrong answer' do
         expect(game_w_questions.answer_current_question!('a')).to be_falsey
+        expect(game_w_questions.status).to eq(:fail)
       end
 
       it 'last on million' do
         game_w_questions.current_level = Question::QUESTION_LEVELS.max
-        expect(game_w_questions.answer_current_question!('d')).to eq(game_w_questions.finished?)
+        expect(game_w_questions.answer_current_question!('d')).to be_truthy
+        expect(game_w_questions.status).to eq(:won)
       end
 
       it 'answer after timeout' do
         game_w_questions.created_at = 1.hour.ago
         expect(game_w_questions.answer_current_question!('d')).to be_falsey
+        expect(game_w_questions.status).to eq(:timeout)
       end
     end
   end
